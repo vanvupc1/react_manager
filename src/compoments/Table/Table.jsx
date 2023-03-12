36c428fef17bc2169c913de8f44809692ad413d5
vanvupc1 <vanvupc1@gmail.com>
2023-03-12 16:44:59 +0700

    12/03/2023

--- src/compoments/Table/Table.jsx
+++ src/compoments/Table/Table.jsx
@@ -1,66 +1,72 @@
-import React from 'react'
-// import img1 from '../../assets/img-anhthe/1.jpg'
-import employee from "../../assets/JsonData/employee_list.json"
- const Employee = (props) =>{
-  return(
-    <tr>
-                <td width="10"><input type="checkbox" name="check1" value="1"/></td>
-                  <td>{props.ID}</td>
-                  <td>{props.Name}</td>
-                  <td><img class="img-card-person" src={props.Image}alt=""/></td>
-                  <td>{props.Address} </td>
-                  <td>{props.Birthday}</td>
-                  <td>{props.Gender}</td>
-                  <td>{props.Phone}</td>
-                  <td>{props.Position}</td>
-                  <td class="table-td-center"><button class="btn btn-primary btn-sm trash" type="button" title="Xóa"
-                      onclick="myFunction(this)"><i class='bx bx-trash' ></i>
-                    </button>
-                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
-                      data-toggle="modal" data-target="#ModalUP"><i class='bx bx-edit' ></i>
-                    </button>
-                  </td>
+import React, { useState } from 'react';
+import employeeList from '../../assets/JsonData/employee_list.json';
 
-                </tr>
-  )
- }
-const Table = props => {
+const Employee = ({ index, employee, onDelete }) => {
+  const handleDeleteRow = () => {
+    onDelete(index);
+  };
+
   return (
-    <table class="table table-hover table-bordered js-copytextarea" cellpadding="0" cellspacing="0" border="0" id="sampleTable">
-       <thead>
-                <tr>
-                  <th width="10"><input type="checkbox" id="all"/></th>
-                  <th>ID nhân viên</th>
-                  <th width="150">Họ và tên</th>
-                  <th width="20">Ảnh thẻ</th>
-                  <th width="300">Địa chỉ</th>
-                  <th>Ngày sinh</th>
-                  <th>Giới tính</th>
-                  <th>SĐT</th>
-                  <th>Chức vụ</th>
-                  <th width="100">Tính năng</th>
-                </tr>
-       </thead>
+    <tr>
+      <td width="10">
+        <input type="checkbox" name="check1" value="1" />
+      </td>
+      <td>{employee.ID}</td>
+      <td>{employee.Name}</td>
+      <td>
+        <img className="img-card-person" src={employee.Image} alt="" />
+      </td>
+      <td>{employee.Address} </td>
+      <td>{employee.Birthday}</td>
+      <td>{employee.Gender}</td>
+      <td>{employee.Phone}</td>
+      <td>{employee.Position}</td>
+      <td className="table-td-center">
+        <button className="btn btn-primary btn-sm trash" type="button" title="Xóa" onClick={handleDeleteRow}>
+          <i className="bx bx-trash"></i>
+        </button>
+        <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class='bx bx-edit' ></i>
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+const Table = () => {
+  const [tableData, setTableData] = useState(employeeList);
+
+  const handleDeleteRow = (index) => {
+    const newData = [...tableData];
+    newData.splice(index, 1);
+    setTableData(newData);
+  };
+
+  return (
+    <table className="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th width="10">
+            <input type="checkbox" id="all" />
+          </th>
+          <th>ID nhân viên</th>
+          <th width="150">Họ và tên</th>
+          <th width="20">Ảnh thẻ</th>
+          <th width="300">Địa chỉ</th>
+          <th>Ngày sinh</th>
+          <th>Giới tính</th>
+          <th>SĐT</th>
+          <th>Chức vụ</th>
+          <th width="100">Tính năng</th>
+        </tr>
+      </thead>
       <tbody>
-                {
-                  employee.map((employee) =>
-                  
-                  <Employee
-                    ID={employee.ID}
-                    Name={employee.Name}
-                    Image={employee.Image}
-                    Address={employee.Address}
-                    Birthday={employee.Birthday}
-                    Gender={employee.Gender}
-                    Phone={employee.Phone}
-                    Position={employee.Position}
-                  />
-                   
-                  )
-                }
+        {tableData.map((employee, index) => (
+          <Employee key={employee.ID} index={index} employee={employee} onDelete={handleDeleteRow} />
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
